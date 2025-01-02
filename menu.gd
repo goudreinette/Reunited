41fd82a53ea21011b9f6871fecc75abc9a0d7ea1
Stephan van der Woerd <stphanvdwoerd@gmail.com>
2025-01-02 18:12:13 +0100

    main menu beginnings

---
+++ menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Walking Scenes/playground.tscn")
+
+
+func _on_credits_pressed() -> void:
+	print("credits")
+	pass # Replace with function body.
