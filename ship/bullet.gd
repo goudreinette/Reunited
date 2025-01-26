85b85f5fc96ddf2235cc5f1237e2de816f991687
Rein van der Woerd <reinvanderwoerd@me.com>
2025-01-26 17:17:14 +0100

    start of clouds shader

--- ship/bullet.gd
+++ ship/bullet.gd
@@ -17,5 +17,5 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 func _on_area_entered(area):
 	if area.is_in_group("enemies"):
-		area.reduce_health(1)
+		#area.reduce_health(1)
 		queue_free()
