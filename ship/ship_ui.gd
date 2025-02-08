6aa0ff4ec3ddaa6493d6b3ee40014a570766e717
Rein van der Woerd <reinvanderwoerd@me.com>
2025-02-08 13:11:05 +0100

    health bar blinks red when taking damage

--- ship/ship_ui.gd
+++ ship/ship_ui.gd
@@ -7,8 +7,24 @@ func update_score(value):
 	score_counter.display_digits(value)
 	
 
+
+var display_shield: float = 100
+var target_shield: float = 100
+var max_shield: float
+
+
 func update_shield(max_value, value):
-	shield_bar.max_value = max_value
-	shield_bar.value = value
-	print(value, max_value)
-	$Healthbar.frame = remap(value, 0, max_value, 0, 52)
+	if value < target_shield:
+		$Healthbar/AnimationPlayer.play("take_damage")
+	target_shield = value
+	max_shield = max_value
+	
+	
+func _process(delta):
+	display_shield = lerp(display_shield, target_shield, 0.05)
+	#shield_bar.max_value = max_value
+	#shield_bar.value = value
+	print(display_shield)
+	$Healthbar.frame = remap(display_shield, 0, max_shield, 0, 52)
+
+	
