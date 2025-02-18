02604b4a2435a8932f91b529c70d2b7421950a9b
Stephan van der Woerd <stphanvdwoerd@gmail.com>
2025-02-18 16:37:42 +0100

    tweaks

--- Walking Scenes/Player/Scripts/Player.gd
+++ Walking Scenes/Player/Scripts/Player.gd
@@ -10,9 +10,12 @@ var can_dash: bool = true
 
 ##animation states
 var state : String = "idle" 
+
 ##vars for falling state
 var is_falling: bool = false
+var above_pit: bool = false
 var is_dead: bool = false
+
 ##setting the respawn point
 @export var respawnnode : Node2D
 var respawnpoint: Vector2
@@ -29,21 +32,20 @@ func _ready() -> void:
 	#Dialogic.start("timeline")
 	
 func _process(delta: float) -> void:
-	
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
 	direction = direction.normalized()
 	
 	if not is_dead and not is_falling:
 		if is_dashing == true: 
-			velocity = direction * dash_speed
+			velocity = cardinal_direction * dash_speed
 		else: 
 			velocity = direction * move_speed
-			
 	else : velocity *= 0.9
 	#if  is_falling : velocity = Vector2(0,0)
 	if SetState() || SetDirection()== true:
 		update_animation()
+	
 	if Input.is_action_just_pressed("dash") and can_dash: 
 		is_dashing = true 
 		$"DashingLines".visible = true
@@ -51,7 +53,12 @@ func _process(delta: float) -> void:
 		can_dash = false
 		$DashRegenTimer.start()
 
-	
+	if above_pit and not is_dashing:
+		if not is_falling and not is_dead :	
+			animation_player.play("falling")
+			print("fall start")
+			is_falling = true
+		
 		
 func  _physics_process(delta ) :
 	move_and_slide()
@@ -67,23 +74,23 @@ func SetDirection() -> bool:
 	if new_dir == cardinal_direction:
 		return false
 	cardinal_direction = new_dir
+	
 	sprite_2d.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true 
 	
 func SetState() -> bool:
 	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
-	## if player is in collider layer 2 new_state = "falling"
+	if is_falling : new_state = "falling"
+	
 	if new_state == state:
 		return false
 	state =	new_state
 	return true
 	
 func update_animation() -> bool:
-	if not is_falling :
-		animation_player.play(state + "_" + anim_direction())
-		pass
+	animation_player.play(state + "_" + anim_direction())
 	return true
-	
+
 func anim_direction() -> String:
 	
 	if cardinal_direction == Vector2.DOWN or direction == Vector2.ZERO:
@@ -94,21 +101,18 @@ func anim_direction() -> String:
 		return "side"
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("entered")
 	#scene_to_load.instantiate()
 	get_tree().change_scene_to_file("res://ship/ship_main.tscn")
 	
-func _on_pit_body_entered(body: Node2D) -> void:
-	if not is_dashing:
-		animation_player.play("falling")
-		is_falling = true
-	pass # Replace with function body.
 	
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "falling" :
-			dead()
-			is_falling = false
-	pass # Replace with function body.
+
+func _on_pit_body_entered(body: Node2D) -> void:
+	above_pit = true
+	
+func _on_pits_body_exited(body: Node2D) -> void:
+	above_pit = false
+
+
 
 func dead() :
 	is_dead = true
@@ -116,11 +120,17 @@ func dead() :
 	$DeathTimer.start()
 	pass
 
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "falling" :
+			is_falling = false
+			dead()
+
 func _on_death_timer_timeout() -> void :
 	if is_dead :
-		is_dead = false
-		visible = true
 		position = respawnpoint
+		is_dead = false
+		above_pit = false
+		visible = true
 		animation_player.play("idle_down")
 		state = "idle"
 
@@ -130,3 +140,7 @@ func _on_dash_timer_timeout() -> void:
 	
 func _on_dash_regen_timer_timeout() -> void:
 	can_dash = true
+
+
+
+			
