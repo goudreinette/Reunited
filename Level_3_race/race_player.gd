097743819912b8bb7c44c8302640495754c5bd05
Rein van der Woerd <reinvanderwoerd@me.com>
2025-03-15 23:50:45 +0100

    Simple race map

---
+++ Level_3_race/race_player.gd
@@ -0,0 +1,16 @@
+class_name RacePlayer extends Camera3D
+
+@export var acceleration: float = 0.2
+@export var top_speed: float = 1
+
+var speed = 0
+@export var drag = 0.9
+
+func _ready():
+	pass
+	
+func _process(delta):
+	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	speed += input.y * acceleration * delta
+	speed *= drag
+	position.z += speed
