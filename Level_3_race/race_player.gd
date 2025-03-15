a279a1eb83de7165ef17bd9a2153c3dd4f0a0984
Rein van der Woerd <reinvanderwoerd@me.com>
2025-03-16 00:07:11 +0100

    Race level: turning!

--- Level_3_race/race_player.gd
+++ Level_3_race/race_player.gd
@@ -2,18 +2,26 @@ class_name RacePlayer extends Camera3D
 
 @export var acceleration: float = 0.2
 @export var top_speed: float = 1
-
 var speed = 0
 @export var drag = 0.9
+
+@export var turn_acceleration: float = 0.5
+var turn_speed = 0
+@export var turn_drag = 0.9
+
 
 func _ready():
 	pass
 	
 func _process(delta):
 	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
 	speed += input.y * acceleration * delta
 	speed *= drag
 	
+	turn_speed += input.x * turn_acceleration * delta
+	turn_speed *= turn_drag
+	
 	translate(Vector3(0, 0, speed))
 	#global_position.z += speed
-	rotation_degrees.y -= input.x * .5
+	rotation_degrees.y -= turn_speed
