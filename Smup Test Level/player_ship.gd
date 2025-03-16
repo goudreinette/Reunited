b1eb624160889f3035b7b688017bf430b7164bf0
Rein van der Woerd <reinvanderwoerd@me.com>
2025-03-16 20:01:14 +0100

    hallucigenia

--- Smup Test Level/player_ship.gd
+++ Smup Test Level/player_ship.gd
@@ -59,7 +59,7 @@ func _process(delta):
 	
 	if Input.is_action_pressed("dodge"):
 		barelling = true
-		$AnimatedSprite2D.play("spin")
+		$AnimatedSprite2D.play("barrel roll")
 		
 		
 	if Input.is_action_pressed("shoot"):
