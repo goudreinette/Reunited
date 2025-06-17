f52d75e3065e6d39cd10dcaccd6609d5831c17ed
Rein van der Woerd <reinvanderwoerd@me.com>
2025-06-17 21:31:06 +0200

    level 1.75 cutscenes

--- Sprites Algemeen/Cutscenes/cutscene.gd
+++ Sprites Algemeen/Cutscenes/cutscene.gd
@@ -10,10 +10,14 @@ var faded_in: bool = false
 
 func _input(event):
 	if event.is_action_pressed("Level trigger q"):
-		play(cutscene_animations[next_animation])
+		if next_animation == cutscene_animations.size():
+			get_tree().change_scene_to_packed(next_level)
+		else:
+			play(cutscene_animations[next_animation])
+
 		next_animation+=1
 
 
+
 func _on_animation_finished(anim_name):
-	if anim_name == cutscene_animations[cutscene_animations.size()-1]:
-		get_tree().change_scene_to_packed(next_level)
+	pass
