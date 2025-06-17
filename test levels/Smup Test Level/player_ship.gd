89c02a5f47bcd1dc0e2160d5ed9b8d212aa29719
Stephan van der Woerd <stphanvdwoerd@gmail.com>
2025-06-18 00:21:15 +0200

    fishes eat you now

--- test levels/Smup Test Level/player_ship.gd
+++ test levels/Smup Test Level/player_ship.gd
@@ -156,6 +156,11 @@ func _on_area_entered(area):
 		area.reduce_health(4)
 		$Hit.play()
 		shield -= 4
+	if area.is_in_group("Angler"):
+		shield -= 100
+		#$Hit.play()
+		print("angler hit")
+		
 	if area.is_in_group("astroids"):
 		area.reduce_health(4)
 		area.explode()
