f5f3e3d755cbf37b470dcb4acea39aac26c012c2
Rein van der Woerd <reinvanderwoerd@me.com>
2025-06-19 09:55:53 +0200

    concert start led brighter, fullscreen and mouseless

--- Sprites Algemeen/Cutscenes/cutscene.gd
+++ Sprites Algemeen/Cutscenes/cutscene.gd
@@ -6,6 +6,10 @@ class_name Cutscene extends AnimationPlayer
 @export var next_level: PackedScene
 
 
+func _init():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+
 func _input(event):
 	if event.is_action_pressed("Level trigger q"):
 		if next_animation == cutscene_animations.size():
